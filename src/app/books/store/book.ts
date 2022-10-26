86ffc76a65458f1fde22cd87e82ad1a904d0c45a
Jay <jayanth.rymec@gmail.com>
2022-10-26 00:23:46 -0600

    created a book interface or store

---
+++ src/app/books/store/book.ts
@@ -0,0 +1,12 @@
+export interface Book {
+  id: number;
+  name: string;
+  author: string;
+  cost: number;
+}
+
+
+// "id": 1,
+// "name": "Harry Potter and the Philosopher's Stone",
+// "author": "J. K. Rowling",
+// "cost": 300
